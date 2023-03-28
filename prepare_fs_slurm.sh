fb780c02d5c07df974303196c4e9f1d770340607
Martin Saint-Jalmes <msaintja@users.noreply.github.com>
2023-03-29 10:36:06 +1100

    refactored scripts

--- prepare_fs_slurm.sh
+++ prepare_fs_slurm.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-mkdir ./.tmp -p
-truncate -s 0 ./.tmp/commands.list
-for subject_img in $(find ./ -name 'sub*nii.gz'); do
+mkdir $1/.tmp -p
+truncate -s 0 $1/.tmp/commands.list
+for subject_img in $(find $1/ -name 'sub*nii.gz'); do
 subject_id=$(awk -F / '{ print $2; }' <<< $subject_img);
-echo "mkdir -p ./derivatives/freesurfer/ && rm -rf ./derivatives/freesurfer/$subject_id && recon-all -all -i $subject_img -s $subject_id -qcache && mkdir -p ./derivatives/fs_minimal/$subject_id/mri/ && mri_convert ./derivatives/freesurfer/$subject_id/mri/brainmask.mgz ./derivatives/fs_minimal/$subject_id/mri/brainmask.nii.gz && mri_convert ./derivatives/freesurfer/$subject_id/mri/brainmask.mgz --apply_transform ./derivatives/freesurfer/$subject_id/mri/transforms/talairach.xfm -o ./derivatives/freesurfer/$subject_id/mri/brainmask_align.mgz && mri_convert ./derivatives/freesurfer/$subject_id/mri/brainmask_align.mgz ./derivatives/fs_minimal/$subject_id/mri/brainmask_align.nii.gz && mri_convert ./derivatives/freesurfer/$subject_id/mri/brain.finalsurfs.mgz ./derivatives/fs_minimal/$subject_id/mri/brain.finalsurfs.nii.gz" >> ./.tmp/commands.list; done
-
-# Ensure that FS directory exists, and that it is empty
-# mkdir ./derivatives/freesurfer -p
-# rm -rf ./derivatives/freesurfer/*
+echo "mkdir -p $1/derivatives/freesurfer/ && rm -rf $1/derivatives/freesurfer/$subject_id && recon-all -all -i $subject_img -s $subject_id -qcache && mkdir -p $1/derivatives/fs_minimal/$subject_id/mri/ && mri_convert $1/derivatives/freesurfer/$subject_id/mri/brainmask.mgz $1/derivatives/fs_minimal/$subject_id/mri/brainmask.nii.gz && mri_convert $1/derivatives/freesurfer/$subject_id/mri/brainmask.mgz --apply_transform $1/derivatives/freesurfer/$subject_id/mri/transforms/talairach.xfm -o $1/derivatives/freesurfer/$subject_id/mri/brainmask_align.mgz && mri_convert $1/derivatives/freesurfer/$subject_id/mri/brainmask_align.mgz $1/derivatives/fs_minimal/$subject_id/mri/brainmask_align.nii.gz && mri_convert $1/derivatives/freesurfer/$subject_id/mri/brain.finalsurfs.mgz $1/derivatives/fs_minimal/$subject_id/mri/brain.finalsurfs.nii.gz" >> $1/.tmp/commands.list; done
